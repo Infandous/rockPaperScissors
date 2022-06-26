bb8371ebe041f1f3cf6c30c3c8ab5cb1dc565731
Infandous <imobrady@ymail.com>
2022-06-26 13:44:03 -0500

    finished creating text based game of RPS

--- game.js
+++ game.js
@@ -39,9 +39,9 @@ function playerSelection(){
 }
 
 //function to play a round of rock paper scissors
-function playGame(){
-    let playerChoice = playerSelection();
-    let computerChoice = computerPlay();
+function playGame(playerChoice,computerChoice){
+    //let playerChoice = playerSelection();
+    //let computerChoice = computerPlay();
     console.log(`The computer chose ${computerChoice}\n`);
 
     //determine if the two are equal
@@ -53,29 +53,67 @@ function playGame(){
     if (playerChoice == "rock"){
         if (computerChoice == "scissors"){
             console.log("Congratulations! rock beats Scissors!");
+            return 1;
         }else if (computerChoice=="paper"){
             console.log("Oops! You'll get 'em next time!");
+            return 0;
         }
-        return;
     }else if (playerChoice=="paper"){
         if (computerChoice=="rock"){
             console.log("You're killing it! Paper beats rock!");
+            return 1;
         }else if (computerChoice == "scissors"){
             console.log("Oops, you've been cut. Better luck next time!");
+            return 0;
         }
-        return;
     }else if (playerChoice=="scissors"){
         if (computerChoice=="paper"){
             console.log("You did it! You cut him up.");
+            return 1;
         }else if (computerChoice=="rock"){
             console.log("Sad! You've been smashed.");
+            return 0;
         }
-        return;
+        
     }else{
         console.log("Hmm...This is weird. We don't know what happened.");
         return;
     }
 
+}
+
+//function to play game as best of 5
+function game(){
+    //player and computer choices
+    let playerChoice; 
+    let computerChoice; 
+    //keep track of scores
+    let playerScore=0;
+    let computerScore=0;
+    //hold return value from each round
+    let roundWinner;
+    //play five rounds of rps
+    for (let i=0; i<5; i++){
+        playerChoice=playerSelection();
+        computerChoice=computerPlay();
+        roundWinner = playGame(playerChoice,computerChoice);
+        //increase score for computer || player
+        if (roundWinner==1){
+            playerScore++;
+        }else{
+            computerScore++;
+        }
+        //break for loop if one player has won
+        if ((playerScore==3)||(computerScore==3)){
+            break;
+        }
+    }
+    //display game winner
+    if (playerScore>computerScore){
+        console.log("Congrats you won!");
+    }else{
+        console.log("Dangit, try again you've got it!");
+    }
 }
 
 
